e00863d9945c90a34dee8b760958365ae2389902
Levan Vekua <lvekua@gmail.com>
2017-04-13 20:18:08 -0400

    another practice iteration

--- js/app.js
+++ js/app.js
@@ -1,127 +1,81 @@
-var todolist = {
+var todoList = {
 
 // NOTE: todo array
     todo: [],
 
-// NOTE: add new items to todo list
-    addItem: function(itemValue){
+// NOTE: display todo items
+    displayTodos: function() {
+        if (this.todo.length === 0) {
+            console.log('EMPTY TODO!');
+        } else{
+            for(var i = 0; i < this.todo.length; i++){
+                if (this.todo[i].completed === true) {
+                    console.log('(X)', this.todo[i].newItemText);
+                } else{
+                    console.log('( )', this.todo[i].newItemText);
+                }
+            }
+        }
+    },
+
+// NOTE: add todo items to todo list
+    addTodo: function(item) {
         this.todo.push({
-            item: itemValue,
+            newItemText: item,
             completed: false
         });
-        view.displayTodos();
+        this.displayTodos();
     },
 
-// NOTE: edit items in todo list
-    editItem: function(itemIndex, itemValue){
-        this.todo[itemIndex].item = itemValue;
-        view.displayTodos();
+// NOTE: edit todo list item
+    editTodo: function(itemIndex, item) {
+        var todo = this.todo[itemIndex];
+        todo.newItemText = item;
+        this.displayTodos();
     },
 
-// NOTE: remove items from todo list
-    removeItem: function(itemIndex){
+// NOTE: delete an item from todo list
+    deleteTodo: function(itemIndex) {
         this.todo.splice(itemIndex, 1);
-        view.displayTodos();
+        this.displayTodos();
     },
 
-// NOTE: check completed items in todo list
-    toggleTodo: function(itemIndex){
-        var itemDone = this.todo[itemIndex];
-        itemDone.completed = !itemDone.completed;
-        view.displayTodos();
+// NOTE: mark todo item as completed
+    markCompletedTodo: function(itemIndex) {
+        var todo = this.todo[itemIndex];
+        todo.completed = !todo.completed;
+        this.displayTodos();
     },
 
-// NOTE: check all items in todo list
-    toggleAll: function(){
-        var totalTodos = this.todo.length;
-        var completedTodos = 0;
+// NOTE: mark all todos as completed ot unmark all todos
+    markAllTodos: function() {
+        var totoalTodos = this.todo.length;
+        var markedTodos = 0;
 
-        for(var i = 0; i < totalTodos; i++){
-            if(this.todo[i].completed === true){
-                completedTodos++
+        for (var i = 0; i < this.todo.length; i++) {
+            if (this.todo[i].completed === true) {
+                markedTodos++
             }
         }
 
-        if(completedTodos === totalTodos){
-            for (var i = 0; i < totalTodos; i++) {
+        if (markedTodos === totoalTodos) {
+            for (var i = 0; i < this.todo.length; i++) {
                 this.todo[i].completed = false;
             }
         } else{
-            for (var i = 0; i < totalTodos; i++) {
+            for (var i = 0; i < this.todo.length; i++) {
                 this.todo[i].completed = true;
             }
         }
-        view.displayTodos();
+        this.displayTodos();
     }
 
 };
 
-var handlers = {
-
-    displayTodos: function(){
-        todolist.displayTodos();
-    },
-    addTodo: function() {
-        var todoItem = document.getElementById('addTodo');
-        todolist.addItem(todoItem.value);
-        todoItem.value = '';
-    },
-    editItem: function(){
-        var itemIndex = document.getElementById('itemIndex');
-        var itemValue = document.getElementById('itemValue');
-        todolist.editItem(itemIndex.valueAsNumber, itemValue.value);
-
-        itemIndex.value = '';
-        itemValue.value = '';
-    },
-    removeItem: function(){
-        var itemPosition = document.getElementById('itemPosition');
-        todolist.removeItem(itemPosition.valueAsNumber);
-
-        itemPosition.value = '';
-    },
-    toggleTodo: function(){
-        var itemToCheck = document.getElementById('itemToCheck');
-        todolist.toggleTodo(itemToCheck.valueAsNumber);
-
-        itemToCheck.value = '';
-    },
-    toggleAll: function(){
-        todolist.toggleAll();
-    }
+var handler = {
 
 };
 
 var view = {
-
-    displayTodos: function(){
-        var todoUl = document.querySelector('ul');
-        var todoTitle = document.getElementById('todoTitle');
-        var todoTitleText = '';
-        todoUl.innerHTML = '';
-
-        if (todolist.todo.length > 0) {
-            todoTitleText = 'TODO ITEMS:';
-        } else{
-            todoTitleText = 'YOUR TODO IS EMPTY!';
-        }
-
-        todoTitle.textContent = todoTitleText
-
-        for (var i = 0; i < todolist.todo.length; i++) {
-            var todoLi = document.createElement('li');
-            var itemValue = todolist.todo[i];
-            var itemText = '';
-
-                if (itemValue.completed === true) {
-                    itemText = '(X) ' + itemValue.item;
-                } else{
-                    itemText = '( ) ' + itemValue.item;
-                }
-
-            todoLi.textContent = itemText;
-            todoUl.appendChild(todoLi);
-        }
-    }
-
+    
 }
